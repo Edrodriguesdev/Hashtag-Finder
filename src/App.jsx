154827ecc9fb9271426b4976d2b4299978e43244
Randal Krebs <68863261+randaltk@users.noreply.github.com>
2020-09-08 16:33:26 -0300

    login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Login from "./pages/Login";
+import "./styles/resetlogin.css";
+import "./styles/login.css";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
